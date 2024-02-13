65b7838906ab9746d11c66de24365bec8199a389
Nico <delagrande.nico@gmail.com>
2024-02-13 12:02:02 +0100

    modification

--- movie-app/src/index.js
+++ movie-app/src/index.js
@@ -1,9 +1,9 @@
-import './index.css';
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client'; // Importez la méthode createRoot depuis 'react-dom/client'
 import App from './App';
 
 const root = createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
